bbda52ce56b83064e681666ad313c116a9ee1333
Anwesh Mishra <anweshm3@gmail.com>
2021-10-12 12:34:05 +0530

    Vowel or Consonant

---
+++ 02-Conditonal and Loops/VowelOrConsonant.java
@@ -0,0 +1,20 @@
+package com.Anwesh;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        System.out.println("Entered Alphabet is Vowel or Consonant");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an Alphabet : ");
+        char alpha = in.next().charAt(0);
+        if ((alpha >= 65 && alpha <= 90) || (alpha >= 97 && alpha <= 122)) {
+            if (alpha == 'A' || alpha == 'a' || alpha == 'E' || alpha == 'e' || alpha == 'I' || alpha == 'i' || alpha == 'O' || alpha == 'o' || alpha == 'U' || alpha == 'u') {
+                System.out.print("Alphabet is Vowel");
+            }
+            else
+                System.out.print("Alphabet is Consonant");
+        }
+        in.close();
+    }
+}

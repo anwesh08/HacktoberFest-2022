22d61d00da8e08c04d48e076c115fd5c9a5519b3
Anwesh Mishra <anweshm3@gmail.com>
2021-10-28 16:28:11 +0530

    Check If It Is a Good Array

---
+++ LeetCode/1250-Good Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isGoodArray(int[] nums) {
+        int x = nums[0], y;
+        for (int a: nums) {
+            while (a > 0) {
+                y = x % a;
+                x = a;
+                a = y;
+            }
+        }
+        return x == 1;
+    }
+}

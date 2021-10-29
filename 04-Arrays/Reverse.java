af7946545515d13fab4c945f50e62c788488b99d
Anwesh Mishra <anweshm3@gmail.com>
2021-10-29 22:31:54 +0530

    Reverse an Array

---
+++ 04-Arrays/Reverse.java
@@ -0,0 +1,25 @@
+package com.Anwesh;
+
+import java.util.Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23, 9, 18};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+    static void swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+}

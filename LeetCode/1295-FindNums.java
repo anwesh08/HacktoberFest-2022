e35fe014915464d0cf151edea2d87c9a879b02b8
Anwesh Mishra <anweshm3@gmail.com>
2021-10-30 19:46:00 +0530

    Find Numbers with Even Number of Digits

--- LeetCode/1295-FindNums.java
+++ LeetCode/1295-FindNums.java
@@ -1,32 +1,19 @@
-package com.Anwesh;
-
-import java.util.Scanner;
-
-public class FindNums {
-    public static void main(String[] args) {
-        System.out.println("Find Numbers with Even Number of Digits");
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter number of elements : ");
-        int n = in.nextInt();
-        System.out.print("Enter array elements : ");
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = in.nextInt();
-        }
-        System.out.println("Number of even digit numbers = " + findNumbers(nums));
-        in.close();
-    }
-    public static int findNumbers(int[] nums) {
-        int count = 0, cnt = 0;
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] > 0) {
-                int rem = nums[i] % 10;
-                nums[i] /= 10;
-                cnt++;
-            }
-            if (cnt % 2 == 0) count++;
-            cnt = 0;
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num: nums) {
+            if(even(num)) count++;
         }
         return count;
+    }
+    static boolean even(int num) {
+        int numOfDigits = digits(num);
+        return numOfDigits % 2 == 0;
+    }
+    static int digits(int num) {
+        if (num < 0) {
+            num *= -1;
+        }
+        return (int)(Math.log10(num) + 1);
     }
 }

ce9de926717558f062ef5a8b5d82434ffd4fc2e1
Anwesh Mishra <anweshm3@gmail.com>
2021-10-31 00:03:21 +0530

    Find First and Last Position of Element in Sorted Array

--- LeetCode/34-Elements in Sorted Array.java
+++ LeetCode/34-Elements in Sorted Array.java
@@ -1,32 +1,14 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = findStartIndex(nums, target);
-        res[1] = findEndIndex(nums, target);
-        return res;
-    }
-    public int findStartIndex(int[] nums, int target) {
-        int index = -1;
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] >= target) end = mid - 1;
-            else start = mid + 1;
-            if (nums[mid] == target) index = mid;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target){
+                for (int j = nums.length-1 ; j >= i ; j--) {
+                    if (nums[j] == target){
+                        return new int[] {i,j};
+                    }
+                }
+            }
         }
-        return index;
-    }
-    public int findEndIndex(int[] nums, int target) {
-        int index = -1;
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] <= target) start = mid + 1;
-            else end = mid - 1;
-            if (nums[mid] == target) index = mid;
-        }
-        return index;
+        return new int[] {-1,-1};
     }
 }

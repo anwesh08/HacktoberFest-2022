9be8b97970eeee9429fc5bd1f7360e267f3dfa28
Anwesh Mishra <anweshm3@gmail.com>
2022-01-04 22:32:14 +0530

    Peak Element

--- LeetCode/162-Peak Element.java
+++ LeetCode/162-Peak Element.java
@@ -1,16 +1,15 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        int low = 0, high = n - 1, mid = 0;
-        if (n == 1) return 0;
-        if (nums[high] > nums[high - 1]) return high;
-        if (nums[low] > nums[low + 1]) return low;
-        while (low < high) {
-            mid = (low + high)/2;
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-            else if (nums[mid] > nums[mid - 1]) low = mid;
-            else high = mid;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else
+                start = mid + 1;
         }
-        return -1;
+        return start;
     } 
 }

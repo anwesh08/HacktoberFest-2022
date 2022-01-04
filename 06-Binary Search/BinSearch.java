e01470f59e8739799736358d322ecdbe31499755
Anwesh Mishra <anweshm3@gmail.com>
2022-01-04 22:36:34 +0530

    Binary Search

---
+++ 06-Binary Search/BinSearch.java
@@ -0,0 +1,24 @@
+package com.Anwesh;
+
+public class BinSearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 16;
+        int ans = binarysearch(arr, target);
+        System.out.println(ans);
+    }
+    static int binarysearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if (target < arr[mid])
+                end = mid - 1;
+            else if (target > arr[mid])
+                start = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}

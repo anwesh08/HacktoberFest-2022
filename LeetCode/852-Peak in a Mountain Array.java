7cd77ab13b6f0edfe664863e318cb9333ef1d9aa
Anwesh Mishra <anweshm3@gmail.com>
2022-01-04 22:34:47 +0530

    Peak Element in Mountain Array

--- LeetCode/852-Peak in a Mountain Array.java
+++ LeetCode/852-Peak in a Mountain Array.java
@@ -1,10 +1,15 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i = 0;
-        while (i < arr.length) {
-            if (arr[i] < arr[i + 1]) i++;
-            else break;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else
+                start = mid + 1;
         }
-        return i;
+        return start;
     }
 }

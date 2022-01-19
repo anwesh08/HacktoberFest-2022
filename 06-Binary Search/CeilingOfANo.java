a7cdcf3e5ec75b007fa8201aea9a1775a07a03e1
Anwesh Mishra <anweshm3@gmail.com>
2022-01-19 19:18:27 +0530

    Ceiling Of A Number

---
+++ 06-Binary Search/CeilingOfANo.java
@@ -0,0 +1,24 @@
+package com.Anwesh;
+
+public class CeilingOfANo {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 21;
+        int ans = Bsearch(arr, target);
+        System.out.println(ans);
+    }
+    static int Bsearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if (arr[mid] > target)
+                end = mid - 1;
+            else if (arr[mid] < target)
+                start = mid + 1;
+            else
+                return mid;
+        }
+        return arr[start];
+    }
+}

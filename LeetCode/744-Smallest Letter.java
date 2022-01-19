749d2f664009b269bd396beaf4c80c8d90913b78
Anwesh Mishra <anweshm3@gmail.com>
2022-01-19 20:42:02 +0530

    Updated Smallest Letter

--- LeetCode/744-Smallest Letter.java
+++ LeetCode/744-Smallest Letter.java
@@ -1,9 +1,14 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int i = 0;
-        for (i = 0; i < letters.length; i++) {
-            if (letters[i] > target) return letters[i];
+        int start = 0;
+        int end = letters.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if (letters[mid] > target)
+                end = mid - 1;
+            else 
+                start = mid + 1;
         }
-        return letters[0];
+        return letters[start % letters.length];
     }
 }

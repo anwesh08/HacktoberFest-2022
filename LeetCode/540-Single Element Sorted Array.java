ba4cb711d6b763298475b493552c724147d4f3e9
Anwesh Mishra <anweshm3@gmail.com>
2022-01-23 10:59:58 +0530

    Single Element Sorted Array

--- LeetCode/540-Single Element Sorted Array.java
+++ LeetCode/540-Single Element Sorted Array.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        if (nums.length == 1) return nums[nums.length-1];
-        else if (nums[nums.length - 1] != nums[nums.length - 2]) return nums[nums.length - 1];
-        else {
-            for (int i = 0; i < nums.length; i += 2) {
-                if (nums[i] != nums[i + 1])
-                    return nums[i];
-            }
+    public int singleNonDuplicate(int[] arr) {
+        int start = 0;
+        int end = arr.length - 2;
+        while (start <= end) {
+            int mid = (start + end) >> 1;
+            if (arr[mid] == arr[mid^1])
+                start = mid + 1;
+            else
+                end = mid - 1;
         }
-        return -1;
+        return arr[start];
     }
 }

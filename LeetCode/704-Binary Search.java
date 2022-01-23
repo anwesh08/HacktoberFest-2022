af88f9484b271a2163d92d233160d8dad6280d08
Anwesh Mishra <anweshm3@gmail.com>
2022-01-23 11:30:06 +0530

    Binary Search

--- LeetCode/704-Binary Search.java
+++ LeetCode/704-Binary Search.java
@@ -1,13 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1, mid = 0;
-        while (low <= high) {
-            mid = (low + high)/2;
-            if (nums[mid] == target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target)
+                end = mid - 1;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else
                 return mid;
-            }
-            else if (target < nums[mid]) high = mid - 1;
-            else low = mid + 1;
         }
         return -1;
     }

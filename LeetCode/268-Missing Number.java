4435a1440e2d205cc00127560a72bea55ac32988
Anwesh Mishra <anweshm3@gmail.com>
2022-02-02 09:17:07 +0530

    Missing Number in an Array in range [0,n]

---
+++ LeetCode/268-Missing Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i])
+                return i;
+        }
+        return nums.length;
+    }
+}

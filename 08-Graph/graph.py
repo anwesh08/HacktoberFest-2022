a3027864fe01e2d8f5694594b2390831407e995e
JessePires <jessepires2010@gmail.com>
2022-10-25 00:14:11 -0300

    feat: Adding class to create graphs
    This class allows to create new graphs and get access to basic
    functions, such as getInDegree, getOutDegree and getDegree.

---
+++ 08-Graph/graph.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+
+class Graph:
+  """Graph class for representing and manipulating graphs"""
+
+  def __init__(self, N):
+    """N is the vertex quantity that'll be added to the graph"""
+
+    self.__adjacencyList = defaultdict(dict)
+
+    for i in range (0, N):
+      self.__adjacencyList[i] = {}
+
+
+  def addEdge(self, u, v, w = 1):
+    """
+      explanation: add an edge to v vertex. \n
+      input: U and V are the vertices. W is the value of the edge. \n
+      condition: if W isn't passed, the edge value is 1. \n
+      return (unsuccessfully): if U and V are non-existent vertices in the graph, a message will be returned. \n
+    """
+
+    if u not in self.__adjacencyList.keys():
+      return "vertex" + str(u) + "doesn't exists"
+
+    if v not in self.__adjacencyList.keys():
+      return "vertex " + str(v) + " doesn't exists"
+
+    else:
+      self.__adjacencyList[u].update({v: w})
+
+
+  def getInDegree(self, v):
+    """
+      explanation: return the in-degree of a vertex.\n
+      input: the vertex V.\n
+      output: the in-degree of a vertex.\n
+    """
+
+    inDegree = 0
+
+    for vertex in self.__adjacencyList.keys():
+      if v in self.__adjacencyList[vertex].keys():
+        inDegree += 1
+
+    return inDegree
+
+
+  def getOutDegree(self, v):
+    """
+      explanation: return the out-degree of a vertex.\n
+      input: the vertex V.\n
+      output: the out-degree of a vertex.\n
+    """
+
+    return len(self.__adjacencyList[v])
+
+
+  def getDegree(self, v):
+    """
+      explanation: return the degree of a vertex.\n
+      input: the vertex V.\n
+      output: the degree of a vertex.\n
+    """
+
+    return self.getOutDegree(v) + self.getInDegree(v)
+    

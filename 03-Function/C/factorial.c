152aa7426a535350bfd7b49b5aafb533e314edf2
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-10-24 23:38:28 -0300

    refactor factorial in c

--- 03-Function/C/factorial.c
+++ 03-Function/C/factorial.c
@@ -2,7 +2,7 @@
 
 int factorial(int n) {
   int fact = 1;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 2; i <= n; i++) {
     fact *= i;
   }
   return fact;
@@ -12,5 +12,5 @@ int main() {
   int n;
   printf("Enter a number: ");
   scanf("%d", &n);
-  printf("Factorial of %d is %d\n", n, factorial(n));
+  printf("%d! = %d\n", n, factorial(n));
 }

f6bd89f7f865a937682b49dfebe1d3033da3afe2
Anwesh Mishra <anweshm3@gmail.com>
2022-10-29 20:55:17 +0530

    class Solution{

---
+++ Geeks For Geeks Practice/Subarray with 0 sum.java
@@ -0,0 +1,18 @@
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        int sum=0;
+        Set<Integer> total=new HashSet<>();
+        total.add(0);
+        for(int i=0;i<n;i++){
+           sum+=arr[i];
+           if(total.contains(sum)){
+               return true;
+           }
+           total.add(sum);
+           
+       }
+       return false;
+    }
+}
